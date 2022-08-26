1007f7465b54fd6723f0ae691d916fbebdbc6015
♠ Levi Perez <levi@leviperez.dev>
2022-08-25 18:25:14 -0700

    added safety catches for SceneLord spam

--- Objects/SceneLord.cs
+++ Objects/SceneLord.cs
@@ -10,8 +10,12 @@ using UnityEngine.SceneManagement;
 
 namespace Ore
 {
+  [CreateAssetMenu(menuName = "Ore/" + nameof(SceneLord), fileName = nameof(SceneLord))]
   public class SceneLord : OAssetSingleton<SceneLord>
   {
+    [SerializeField]
+    protected UnloadSceneOptions m_DefaultUnloadSceneOptions = UnloadSceneOptions.None;
+    
     
     [PublicAPI]
     public static void AppQuit()
@@ -20,30 +24,50 @@ namespace Ore
     }
 
     [PublicAPI]
-    public static void LoadSceneAdditiveAsync(int build_idx)
+    public static void LoadSceneAdditiveAsync(int index)
     {
-      var load = SceneManager.LoadSceneAsync(build_idx, LoadSceneMode.Additive);
+      var load = SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
       load.allowSceneActivation = true;
     }
 
     [PublicAPI]
-    public static void LoadSceneAsync(int build_idx)
+    public static void LoadSceneAsync(int index)
     {
-      var load = SceneManager.LoadSceneAsync(build_idx, LoadSceneMode.Additive);
+      if (s_CurrentLoadAsync == index)
+        return;
+
+      if (-1 < s_CurrentLoadAsync)
+      {
+        Orator.WarnOnce($"Already loading scene #{s_CurrentLoadAsync}, can't queue another one!", Current);
+        return;
+      }
       
+      s_CurrentLoadAsync = index;
+
+      index = SceneManager.sceneCount; // now this is runtime index, not build index
+
+      var load = SceneManager.LoadSceneAsync(s_CurrentLoadAsync, LoadSceneMode.Additive);
+
       load.completed += _ =>
       {
+        s_CurrentLoadAsync = -1;
         var curr = SceneManager.GetActiveScene();
-        var next = SceneManager.GetSceneAt(build_idx);
+        var next = SceneManager.GetSceneAt(index);
         if (curr != next)
         {
           SceneManager.SetActiveScene(next);
-          SceneManager.UnloadSceneAsync(curr, UnloadSceneOptions.None);
+          if (IsActive)
+            SceneManager.UnloadSceneAsync(curr, Current.m_DefaultUnloadSceneOptions);
+          else
+            SceneManager.UnloadSceneAsync(curr, UnloadSceneOptions.None);
         }
       };
       
       load.allowSceneActivation = true;
     }
+    
+    
+    private static int s_CurrentLoadAsync = -1;
 
   } // end class SceneLord
 }
