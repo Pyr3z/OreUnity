2bd2873637072ac5fadb86aeb748268b16d50b3d
♠ Levi Perez <levi@leviperez.dev>
2022-09-26 17:06:58 -0700

    Raster.LineDrawer now provides Diagonal distance; fixed ray length issue

--- Static/Raster.cs
+++ Static/Raster.cs
@@ -5,8 +5,12 @@
 
 using System.Collections;
 using System.Collections.Generic;
+
 using JetBrains.Annotations;
+
 using UnityEngine;
+
+using Math = System.Math;
 
 
 namespace Ore
@@ -20,31 +24,81 @@ namespace Ore
     /// </summary>
     public struct LineDrawer : IEnumerator<Vector2Int>, IEnumerable<Vector2Int>
     {
+      [PublicAPI]
       public Vector2Int Current => new Vector2Int(x, y);
-      object IEnumerator.Current => Current;
+      [PublicAPI]
+      public Vector2Int Start   => new Vector2Int(sx, sy);
 
-      private int sx, sy, x, y, dx, dy, xinc, yinc, i, error, side, state;
+      [PublicAPI]
+      public float EuclideanDistance => Vector2Int.Distance(Start, Current);
+      [PublicAPI]
+      public float ManhattanDistance => D * (xinc * (x - sx) + yinc * (y - sy));
+      [PublicAPI]
+      public float DiagonalDistance
+      {
+        get
+        {
+          float dx = xinc * (x - sx);
+          float dy = yinc * (y - sy);
+
+          if (dy < dx)
+            return D * (dx - dy) + D2 * dy;
+          else
+            return D * (dy - dx) + D2 * dx;
+        }
+      }
+
+
+      public int x, y;
+      
+      private int sx, sy, dx, dy, xinc, yinc, i, error, side, state;
+
+      private RectInt m_Bounds;
+
 
       private const int PREPARED  = 0;
       private const int ITERATING = 1;
 
-      private RectInt m_Bounds;
+      private const float D  = 1f;
+      private const float D2 = Floats.SQRT2;
+
 
 
       [PublicAPI]
       public static LineDrawer WithBounds(int xmin, int ymin, int xmax, int ymax)
       {
+        if (xmax < xmin)
+          (xmin,xmax) = (xmax,xmin);
+        if (ymax < ymin)
+          (ymin,ymax) = (ymax,ymin);
+
         return new LineDrawer
         {
           m_Bounds = new RectInt(xmin, ymin, xmax - xmin, ymax - ymin)
         };
       }
 
+
       [PublicAPI]
       public void Prepare(int ax, int ay, int bx, int by)
       {
         x = sx = ax;
         y = sy = ay;
+
+        if (m_Bounds.width > 0)
+        {
+          int xmin = m_Bounds.xMin, ymin = m_Bounds.yMin,
+              xmax = m_Bounds.xMax, ymax = m_Bounds.yMax;
+
+          // invalid if starting out of bounds:
+          if (ax < xmin || ax >= xmax || ay < ymin || ay >= ymax)
+          {
+            x = xmin;
+            y = ymin;
+            i = 0;
+            return;
+          }
+        }
 
         xinc = (bx < ax) ? -1 : 1;
         yinc = (by < ay) ? -1 : 1;
@@ -71,30 +125,8 @@ namespace Ore
       [PublicAPI]
       public void Prepare(Vector2 start, Vector2 direction, float distance)
       {
-        x = sx = (int)start.x;
-        y = sy = (int)start.y;
-
-        xinc = direction.x < 0f ? -1 : 1;
-        yinc = direction.y < 0f ? -1 : 1;
-
-        dx = (int)(xinc * direction.x * distance);
-        dy = (int)(yinc * direction.y * distance);
-
-        if (dx == dy)
-        {
-          i = dx;
-        }
-        else
-        {
-          side  = -1 * ((dx == 0 ? yinc : xinc) - 1);
-          i     = dx + dy;
-          error = dx - dy;
-        }
-
-        dx *= 2;
-        dy *= 2;
-
-        state = PREPARED;
+        var b = start + direction * distance;
+        Prepare((int)start.x, (int)start.y, (int)b.x, (int)b.y);
       }
 
 
@@ -129,27 +161,38 @@ namespace Ore
           error += dx;
         }
 
-        if (m_Bounds.width > 0 && !m_Bounds.Contains(new Vector2Int(x, y)))
+        if (m_Bounds.width == 0)
+          return true;
+
+        if (x < m_Bounds.x || x >= m_Bounds.xMax)
         {
-          i = 0;
-          return false;
+          x -= xinc;
+          i = -1;
         }
 
-        return true;
+        if (y < m_Bounds.y || y >= m_Bounds.yMax)
+        {
+          y -= yinc;
+          i = -1;
+        }
+
+        return i >= 0;
       }
 
       public void Reset()
       {
-        x = sx;
-        y = sy;
+        x     = sx;
+        y     = sy;
         i     = (dx + dy) / 2;
         error = (dx - dy) / 2;
         state = PREPARED;
       }
 
+
+      object IEnumerator.Current => Current;
+
       // this trick allows this to be used in a foreach loop:
       IEnumerator<Vector2Int> IEnumerable<Vector2Int>.GetEnumerator() => this;
-
       IEnumerator IEnumerable.GetEnumerator() => this;
 
       void System.IDisposable.Dispose()
