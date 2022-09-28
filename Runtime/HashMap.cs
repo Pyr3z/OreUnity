a7dc16e12bfb67e418098dd1bb212ee934ee4e4a
♠ Levi Perez <levi@leviperez.dev>
2022-09-28 13:54:56 -0700

    added stubs for the long-awaited HashMap impl

---
+++ Runtime/HashMap.cs
@@ -0,0 +1,20 @@
+/*! @file       Runtime/HashMap.cs
+ *  @author     Levi Perez (levi\@leviperez.dev)
+ *  @date       2022-09-28
+**/
+
+using System.Collections.Generic;
+using System.Collections;
+
+
+namespace Ore
+{
+
+  public class HashMap
+  {
+
+
+
+  } // end class HashMap
+
+}
---
+++ Runtime/HashMapParams.cs
@@ -0,0 +1,15 @@
+/*! @file       Runtime/HashMapParams.cs
+ *  @author     Levi Perez (levi\@leviperez.dev)
+ *  @date       2022-09-28
+**/
+
+
+namespace Ore
+{
+  public class HashMapParams
+  {
+
+
+
+  } // end class HashMapParams
+}
