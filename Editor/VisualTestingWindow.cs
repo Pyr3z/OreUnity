d1da314e1401d73befa61c0ec1d6123a371d2f64
♠ Levi Perez <levi@leviperez.dev>
2022-10-31 06:39:46 -0700

    epic hashmap visualizer :D

--- Editor/VisualTestingWindow.cs
+++ Editor/VisualTestingWindow.cs
@@ -3,6 +3,7 @@
  *  @date       2022-10-18
 **/
 
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
@@ -13,7 +14,7 @@ using EGU = UnityEditor.EditorGUIUtility;
 
 namespace Ore.Editor
 {
-  public class VisualTestingWindow : EditorWindow
+  public class VisualTestingWindow : EditorWindow, ISerializationCallbackReceiver
   {
 
     [MenuItem("Ore/Tools/Visual Testing (Ore)")]
@@ -71,6 +72,17 @@ namespace Ore.Editor
     [SerializeField]
     private float m_CircleRadiusBias = Raster.CircleDrawer.RADIUS_BIAS;
 
+    [SerializeField]
+    private int[] m_HashMapKeys;
+    [SerializeField]
+    private string[] m_HashMapValues;
+
+    [System.NonSerialized]
+    private readonly HashMap<int,string> m_HashMap = new HashMap<int, string>();
+
+    [System.NonSerialized]
+    private GUIStyle m_SceneLabelStyle;
+
 
     private void OnBecameVisible()
     {
@@ -89,6 +101,31 @@ namespace Ore.Editor
       SceneView.duringSceneGui -= OnSceneGUI;
     }
 
+    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    {
+      m_HashMapKeys = new int[m_HashMap.Count];
+      m_HashMapValues = new string[m_HashMap.Count];
+
+      int i = 0;
+      foreach (var (key,val) in m_HashMap)
+      {
+        m_HashMapKeys[i] = key;
+        m_HashMapValues[i] = val;
+        ++ i;
+      }
+    }
+
+    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    {
+      m_HashMap.Clear();
+
+      int i = m_HashMapKeys?.Length ?? 0;
+      while (i --> 0)
+      {
+        m_HashMap[m_HashMapKeys[i]] = m_HashMapValues[i];
+      }
+    }
+
 
     private void OnGUI()
     {
@@ -97,7 +134,7 @@ namespace Ore.Editor
       if (!m_Foldout)
         return;
 
-      OGUI.LabelWidth.PushDelta(-50f);
+      OGUI.LabelWidth.Push(EGU.currentViewWidth * 0.333f);
 
       EGL.Space();
 
@@ -118,6 +155,7 @@ namespace Ore.Editor
         case Mode.RasterLine:     RasterLineInspector();    break;
         case Mode.RasterCircle:   RasterCircleInspector();  break;
         case Mode.ColorAnalysis:  ColorAnalysisInspector(); break;
+        case Mode.HashMaps:       HashMapsInspector();      break;
 
         default:
           EGL.SelectableLabel("(there's nothing else here...)");
@@ -133,16 +171,30 @@ namespace Ore.Editor
       if (m_Mode == Mode.None || view != SceneView.lastActiveSceneView)
         return;
 
+      if (m_SceneLabelStyle is null)
+      {
+        m_SceneLabelStyle = new GUIStyle(EditorStyles.miniLabel)
+        {
+          alignment = TextAnchor.MiddleCenter,
+          clipping  = TextClipping.Overflow,
+          fontSize  = 9,
+          normal =
+          {
+            textColor = m_PrimaryColor
+          }
+        };
+      }
+
       view.sceneViewState.alwaysRefresh = true;
 
       var cam = view.camera;
-      var min = cam.ViewportToWorldPoint(new Vector2(0f, 0f));
-      var max = cam.ViewportToWorldPoint(new Vector2(1f, 1f));
+      var min = cam.ViewportToWorldPoint(new Vector2(0.01f, 0.01f));
+      var max = cam.ViewportToWorldPoint(new Vector2(0.95f, 0.95f));
       var visible = new RectInt(
         xMin:   (int) min.x,
         yMin:   (int) min.y, 
-        width:  (int)(max.x - min.x + 1.5f),
-        height: (int)(max.y - min.y + 1.5f)
+        width:  (int)(max.x - min.x),
+        height: (int)(max.y - min.y)
       );
 
       var mouse = Event.current.mousePosition;
@@ -157,6 +209,10 @@ namespace Ore.Editor
       else if (m_Mode == Mode.RasterCircle)
       {
         RasterCircleSceneGUI(visible, mouse);
+      }
+      else if (m_Mode == Mode.HashMaps)
+      {
+        HashMapsSceneGUI(visible, mouse);
       }
     }
 
@@ -220,13 +276,13 @@ namespace Ore.Editor
         distance = m_Length;
       }
 
-      var tile = new Rect(0f, 0f, 1f, 1f);
-      var line = new Raster.LineDrawer().Prepare(start, direction, distance);
-
       using (new Handles.DrawingScope(m_PrimaryColor.Inverted()))
       {
         Handles.DrawLine(new Vector2((int)start.x, (int)start.y), mouse);
       }
+
+      var tile = new Rect(0f, 0f, 1f, 1f);
+      var line = new Raster.LineDrawer().Prepare(start, direction, distance);
 
       while (line.MoveNext())
       {
@@ -385,7 +441,7 @@ namespace Ore.Editor
 
       EGL.Space();
 
-      m_Length = EditorGUILayout.Slider("Fill Bar %", m_Length, 0f, 1f);
+      m_Length = EGL.Slider("Fill Bar %", m_Length, 0f, 1f);
 
       OGUI.Draw.FillBar(m_Length, fill: m_PrimaryColor, textColor: m_SecondaryColor);
 
@@ -397,7 +453,178 @@ namespace Ore.Editor
 
     private void HashMapsInspector()
     {
+      EGL.LabelField("Internal Size:", m_HashMap.Buckets.Length.ToString());
 
+      EGL.BeginHorizontal();
+      EGL.LabelField("Grow Threshold:", m_HashMap.Capacity.ToString());
+      float ratio = m_HashMap.Parameters.LoadFactor;
+      OGUI.Draw.FillBar(ratio);
+      EGL.EndHorizontal();
+
+      EGL.BeginHorizontal();
+      EGL.LabelField("Current Count:", m_HashMap.Count.ToString());
+      ratio = (float)m_HashMap.Count / m_HashMap.Buckets.Length;
+      OGUI.Draw.FillBar(ratio);
+      EGL.EndHorizontal();
+
+      EGL.BeginHorizontal();
+      EGL.LabelField("Hash Collisions:", m_HashMap.Collisions.ToString());
+      ratio = (float)m_HashMap.Collisions / m_HashMap.Buckets.Length;
+      if (ratio > m_HashMap.Parameters.LoadFactor / 2f)
+        OGUI.Draw.FillBar(ratio, fill: Colors.Attention);
+      else if (ratio > 0.2f)
+        OGUI.Draw.FillBar(ratio, fill: Colors.Pending);
+      else
+        OGUI.Draw.FillBar(ratio);
+      EGL.EndHorizontal();
+
+      EGL.BeginHorizontal();
+      if (GUILayout.Button("Clear"))
+      {
+        m_HashMap.Clear();
+      }
+      if (GUILayout.Button("Reset Capacity"))
+      {
+        m_HashMap.ResetCapacity();
+      }
+      if (GUILayout.Button("Rehash"))
+      {
+        m_HashMap.Rehash();
+      }
+      EGL.EndHorizontal();
+
+      if (GUILayout.Button("Map random int->string"))
+      {
+        m_HashMap.Map(Integers.RandomIndex(Primes.MaxValue), Colors.Random().ToHex());
+      }
+
+      OGUI.IndentLevel.Push(0);
+      OGUI.LabelWidth.Push(55f);
+
+      var editQueue = new Queue<(int k,string v)>();
+
+      for (int i = 0; i < m_HashMap.Buckets.Length; ++i)
+      {
+        var bucket = m_HashMap.Buckets[i];
+
+        if (bucket.Key == default)
+          continue;
+
+        EGL.BeginHorizontal();
+
+        if (bucket.DirtyHash < 0)
+          OGUI.ScratchContent.text = $"<color=#{m_SecondaryColor.ToHex()}>slot {i}:</color>";
+        else
+          OGUI.ScratchContent.text = $"slot {i}:";
+
+        EG.BeginChangeCheck();
+        int editKey = EGL.DelayedIntField(OGUI.ScratchContent, bucket.Key);
+        if (EG.EndChangeCheck())
+        {
+          editQueue.Enqueue((bucket.Key,null));
+          editQueue.Enqueue((editKey,bucket.Value));
+        }
+
+        EG.BeginChangeCheck();
+        string edit = EGL.DelayedTextField(bucket.Value);
+        if (EG.EndChangeCheck())
+        {
+          editQueue.Enqueue((bucket.Key, edit));
+        }
+
+        EGL.EndHorizontal();
+      }
+
+      while (editQueue.Count > 0)
+      {
+        var (key,val) = editQueue.Dequeue();
+        if (val.IsEmpty())
+        {
+          m_HashMap.Unmap(key);
+        }
+        else
+        {
+          m_HashMap[key] = val;
+        }
+      }
+
+      OGUI.LabelWidth.Pop();
+      OGUI.IndentLevel.Pop();
+    }
+
+    private void HashMapsSceneGUI(RectInt visible, Vector2 mouse)
+    {
+      const float kOffset = 0.015f;
+
+      visible.xMax -= 1;
+      visible.yMax -= 3;
+
+      var tile = new Rect(
+        visible.xMin + kOffset,
+        visible.yMax + kOffset,
+        1f - 2 * kOffset,
+        1f - 2 * kOffset
+      );
+
+      var guiSize = HandleUtility.WorldToGUIPoint(tile.position + Vector2.one);
+      guiSize -= HandleUtility.WorldToGUIPoint(tile.position);
+      guiSize.y *= -1;
+
+      for (int i = 0; i < m_HashMap.Buckets.Length; ++i)
+      {
+        var bucket = m_HashMap.Buckets[i];
+
+        Color32 fill, outline;
+        if (bucket.DirtyHash == default && bucket.Key == default)
+        {
+          fill = Colors.Clear;
+          outline = Colors.Boring;
+        }
+        else if (bucket.DirtyHash < 0)
+        {
+          m_SceneLabelStyle.normal.textColor = m_SecondaryColor;
+
+          if (bucket.Value is null)
+          {
+            fill = Colors.Clear;
+            outline = m_SecondaryColor;
+          }
+          else
+          {
+            fill = Colors.Boring;
+            outline = m_SecondaryColor;
+          }
+        }
+        else
+        {
+          fill = Colors.Boring;
+          outline = m_PrimaryColor;
+        }
+
+        Handles.DrawSolidRectangleWithOutline(tile, fill, outline);
+
+        Handles.BeginGUI();
+
+        GUI.Label(
+          new Rect(HandleUtility.WorldToGUIPoint(tile.position + Vector2.up), guiSize), 
+          $"{i:00}",
+          m_SceneLabelStyle
+        );
+
+        Handles.EndGUI();
+
+        m_SceneLabelStyle.normal.textColor = m_PrimaryColor;
+
+        if ((int)tile.x == visible.xMax)
+        {
+          tile.x = visible.xMin + kOffset;
+          tile.y -= 1f;
+        }
+        else
+        {
+          tile.x += 1f;
+        }
+      }
     }
 
   } // end class VisualTestingWindow
