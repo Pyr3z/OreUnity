e91354f1f4ce1f727d5e606ba5ca295b1ad4a546
♠ Levi Perez <levi@leviperez.dev>
2023-01-04 12:11:53 -0800

    Added some aggressive inlining & jetbrains annotations to Lists class

--- Static/Lists.cs
+++ Static/Lists.cs
@@ -3,9 +3,12 @@
  *  @date       2022-06-20
 **/
 
-using System.Collections;
-using System.Collections.Generic;
 using JetBrains.Annotations;
+
+using System.Collections.Generic;
+
+using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
+using MethodImplOptions   = System.Runtime.CompilerServices.MethodImplOptions;
 
 
 namespace Ore
@@ -17,26 +20,29 @@ namespace Ore
   public static class Lists
   {
 
-    public static bool IsEmpty<T>(this ICollection<T> list)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsEmpty<T>([CanBeNull] this ICollection<T> list)
     {
       return list == null || list.Count == 0;
     }
 
 
-    public static void PushFront<T>(this IList<T> list, T item)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void PushFront<T>([NotNull] this IList<T> list, T item)
     {
-      OAssert.NotNull(list, nameof(list));
+      // OAssert.NotNull(list, nameof(list));
       list.Insert(0, item);
     }
 
-    public static void PushBack<T>(this IList<T> list, T item)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void PushBack<T>([NotNull] this IList<T> list, T item)
     {
-      OAssert.NotNull(list, nameof(list));
+      // OAssert.NotNull(list, nameof(list));
       list.Insert(list.Count, item);
     }
 
 
-    public static T PopFront<T>(this IList<T> list)
+    public static T PopFront<T>([NotNull] this IList<T> list)
     {
       OAssert.NotEmpty(list, nameof(list));
 
@@ -45,7 +51,7 @@ namespace Ore
       return result;
     }
 
-    public static T PopBack<T>(this IList<T> list)
+    public static T PopBack<T>([NotNull] this IList<T> list)
     {
       OAssert.NotEmpty(list, nameof(list));
 
@@ -54,14 +60,16 @@ namespace Ore
       return result;
     }
 
-    public static T[] PopBack<T>(this IList<T> list, int count)
+    [NotNull]
+    public static T[] PopBack<T>([NotNull] this IList<T> list, int count)
     {
       OAssert.NotNull(list, nameof(list));
+      OAssert.True(count > 0, "count > 0");
 
       var result = new T[count];
 
       int i = 0, j = list.Count;
-      while (count-- > 0 && j > 0)
+      while (count --> 0 && j > 0)
       {
         result[i++] = list[--j];
         list.RemoveAt(j);
@@ -71,20 +79,16 @@ namespace Ore
     }
 
 
-    public static T Front<T>(this IReadOnlyList<T> list, T fallback = default)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T Front<T>([NotNull] this IReadOnlyList<T> list, T fallback = default)
     {
-      if (list == null || list.Count == 0)
-        return fallback;
-
-      return list[0];
+      return list.Count == 0 ? fallback : list[0];
     }
 
-    public static T Back<T>(this IReadOnlyList<T> list, T fallback = default)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static T Back<T>([NotNull] this IReadOnlyList<T> list, T fallback = default)
     {
-      if (list == null || list.Count == 0)
-        return fallback;
-
-      return list[list.Count - 1];
+      return list.Count == 0 ? fallback : list[list.Count - 1];
     }
 
 
@@ -136,6 +140,7 @@ namespace Ore
       }
     }
 
+    [NotNull]
     public static IList<T> Shuffled<T>([NotNull] this IList<T> list)
     {
       var copy = new List<T>(list);
