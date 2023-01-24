34d0ae2f9085838da38da180ba46a9be6de09093
♠ Levi Perez <levi@leviperez.dev>
2023-01-24 15:05:07 -0800

    Bitwise+CTZ.cs: Pasted in un-copyright terms from original code

--- Static/Bitwise+CTZ.cs
+++ Static/Bitwise+CTZ.cs
@@ -2,14 +2,46 @@
  *  @author levianperez\@gmail.com
  *  @author levi\@leviperez.dev
  *  @date   2020-06-06
+ *  @date   2023-01-24 (removed alt implementation; added XML docs)
  *
  *  @brief
- *    Implements [CTZ](https://en.wikipedia.org/wiki/Find_first_set)
- *    using precomputed "De Bruijn" lookups I generated in C.
+ *    Implements Count Trailing Zeroes, a slight variation of the
+ *    [Find First Set](https://en.wikipedia.org/wiki/Find_first_set) bitwise
+ *    instruction\. This is useful for reversing bitmasks and being able to loop
+ *    through their non-zero bit indices.
  *
- *  @remark
- *    If you supply the preprocessor define "BITWISE_CTZ_NOJUMP", this
- *    implementation will swap to using a shittier algorithm! :D
+ *  @remarks
+ *    Implementation uses a precomputed "De Bruijn" lookups Levi generated in C.
+ *    Source: https://github.com/Pyr3z/hurry-c-lookupgen
+ *
+ *  @copyright
+ *    This code was released into the Public Domain by Levi Perez in June 2020.
+ *    The declaration was under the following terms, provided for convenience:
+ *
+ *      This is free and unencumbered software released into the public domain.
+ *
+ *      Anyone is free to copy, modify, publish, use, compile, sell, or
+ *      distribute this software, either in source code form or as a compiled
+ *      binary, for any purpose, commercial or non-commercial, and by any
+ *      means.
+ *
+ *      In jurisdictions that recognize copyright laws, the author or authors
+ *      of this software dedicate any and all copyright interest in the
+ *      software to the public domain. We make this dedication for the benefit
+ *      of the public at large and to the detriment of our heirs and
+ *      successors. We intend this dedication to be an overt act of
+ *      relinquishment in perpetuity of all present and future rights to this
+ *      software under copyright law.
+ *      
+ *      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *      IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *      OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *      ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *      OTHER DEALINGS IN THE SOFTWARE.
+ *      
+ *      For more information, please refer to <http://unlicense.org/>
 **/
 
 using MethodImplAttribute = System.Runtime.CompilerServices.MethodImplAttribute;
