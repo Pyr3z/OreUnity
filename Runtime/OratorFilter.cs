dd260dcccaa72b1e8d623f4b9763a4c8709e6e0e
♠ Levi Perez <levi@leviperez.dev>
2023-03-08 11:28:52 -0800

    implemented OratorFilter with flags, inversion, and regex

--- Runtime/OratorFilter.cs
+++ Runtime/OratorFilter.cs
@@ -12,13 +12,65 @@ namespace Ore
 {
 
   [System.Serializable]
-  public struct OratorFilter
+  public struct OratorFilter : ISerializationCallbackReceiver
   {
 
-    public LogTypeFlags Flags;
+    [SerializeField]
+    public bool Invert;
+
+    [SerializeField]
+    public LogTypeFlags Types;
+
+    public string MessageRegex
+    {
+      get => m_MessageRegex;
+      set
+      {
+        m_MessageRegex = value;
+        RecompileRegex();
+      }
+    }
+
+    [SerializeField, Delayed]
+    private string m_MessageRegex;
+
 
     [System.NonSerialized]
-    public Regex MessageFilter;
+    private Regex m_Regex;
+
+    private const RegexOptions REGEX_OPTS = RegexOptions.Compiled   |
+                                            RegexOptions.Singleline |
+                                            RegexOptions.CultureInvariant;
+
+
+    public bool Filters(LogType type, string message)
+    {
+      int flag = 1 << (int)type;
+      return Invert ^ ( ((int)Types & flag) == flag &&
+                        ( m_Regex is null || m_Regex.IsMatch(message) ) );
+    }
+
+
+    private void RecompileRegex()
+    {
+      if (m_MessageRegex.IsEmpty())
+      {
+        m_Regex = null;
+      }
+      else
+      {
+        m_Regex = new Regex(m_MessageRegex, REGEX_OPTS);
+      }
+    }
+
+    void ISerializationCallbackReceiver.OnAfterDeserialize()
+    {
+      RecompileRegex();
+    }
+
+    void ISerializationCallbackReceiver.OnBeforeSerialize()
+    {
+    }
 
   } // end struct OratorFilter
 
