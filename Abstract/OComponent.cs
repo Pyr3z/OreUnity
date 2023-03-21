d23b438fedd9b86b33e90f3198e068f7d23a2b0b
♠ Levi Perez <levi@leviperez.dev>
2023-03-21 14:41:45 -0700

    Asset,OComponent: code cleanup, complete XML dox

--- Abstract/OComponent.cs
+++ Abstract/OComponent.cs
@@ -21,64 +21,114 @@ namespace Ore
   public abstract class OComponent : MonoBehaviour
   {
 
-    [PublicAPI]
+    /// <summary>
+    ///   Debug log a message, with context, from virtually anywhere :O
+    /// </summary>
+    ///
+    /// <remarks>
+    ///   The purpose of this method—as well as similar methods provided in this
+    ///   base class—is mainly to serve as a convenience utility when working
+    ///   with Object references in serialized
+    ///   <see cref="UnityEngine.Events.UnityEvent"/>s.
+    /// </remarks>
     [System.Diagnostics.Conditional("DEBUG")]
-    public void DebugLog(string message)
+    public void DebugLog([CanBeNull] string message)
     {
       Orator.Log(message, this);
     }
 
 
-    [PublicAPI]
-    public void SpawnLocal(GameObject prefab)
+    /// <summary>
+    ///   Spawns a new GameObject and transforms the original prefab's pose into
+    ///   the local space of this component's GameObject (without childing,
+    ///   ignoring scale).
+    /// </summary>
+    ///
+    /// <inheritdoc cref="DebugLog"/>
+    public void SpawnLocal([CanBeNull] GameObject prefab)
     {
       // DISCLAIMER: SpawnPools would be FAR better to use instead of this!
       if (prefab)
         Instantiate(prefab, transform.position, transform.rotation * prefab.transform.rotation);
-      else
-        Debug.LogWarning($"{GetType().Name} \"{name}\" : Missing Prefab reference!");
     }
 
-    [PublicAPI]
-    public void SpawnWorld(GameObject prefab)
+    /// <summary>
+    ///   Spawns a new GameObject with the original prefab's transform.
+    /// </summary>
+    ///
+    /// <inheritdoc cref="SpawnLocal"/>
+    public void SpawnWorld([CanBeNull] GameObject prefab)
     {
       // DISCLAIMER: SpawnPools would be FAR better to use instead of this!
       if (prefab)
         Instantiate(prefab);
-      else
-        Debug.LogWarning($"{GetType().Name} \"{name}\" : Missing Prefab reference!");
     }
 
 
-    [PublicAPI]
+    /// <summary>
+    ///   Flips this component's <see cref="Behaviour.enabled"/> state.
+    /// </summary>
+    ///
+    /// <inheritdoc cref="DebugLog"/>
     public void ToggleSelf()
     {
       enabled = !enabled;
     }
 
-    [PublicAPI]
+    /// <summary>
+    ///   Flips this component's GameObject's <see cref="GameObject.activeSelf"/>
+    ///   state.
+    /// </summary>
+    ///
+    /// <inheritdoc cref="ToggleSelf"/>
     public void ToggleGameObject()
     {
       gameObject.SetActive(!gameObject.activeSelf);
     }
 
 
-    [PublicAPI]
-    public void DestroySelf(float inSeconds = 0f)
+    /// <summary>
+    ///   Destroys this component relatively immediately, thereby removing it
+    ///   from its owning GameObject's list of components.
+    /// </summary>
+    ///
+    /// <inheritdoc cref="DestroyOther"/>
+    public void DestroySelf()
     {
-      if (Application.isEditor && inSeconds.ApproximatelyZero())
-        DestroyImmediate(this);
-      else
-        Destroy(this, inSeconds);
+      this.Destroy();
     }
 
-    [PublicAPI]
-    public void DestroyGameObject(float inSeconds = 0f)
+    /// <summary>
+    ///   Destroys the GameObject that owns this component relatively
+    ///   immediately, thereby also destroying this component and any/all other
+    ///   childed components and GameObjects.
+    /// </summary>
+    ///
+    /// <seealso cref="UnityObjects.Destroy(Object)">
+    ///   Ore.UnityObjects.Destroy(...)
+    /// </seealso>
+    ///
+    /// <inheritdoc cref="DebugLog"/>
+    public void DestroyGameObject()
     {
-      if (Application.isEditor && inSeconds.ApproximatelyZero())
-        DestroyImmediate(gameObject);
-      else
-        Destroy(gameObject, inSeconds);
+      gameObject.Destroy();
+    }
+
+    /// <summary>
+    ///   Destroys the given object relatively immediately, if it exists. <br/>
+    ///   If executed in-editor, any associated serialized asset will also be
+    ///   destroyed.
+    /// </summary>
+    ///
+    /// <seealso cref="UnityObjects.Destroy(Object)">
+    ///   Ore.UnityObjects.Destroy(...)
+    /// </seealso>
+    ///
+    /// <inheritdoc cref="DebugLog"/>
+    public void DestroyOther([CanBeNull] Object other)
+    {
+      if (other)
+        other.Destroy();
     }
 
 
