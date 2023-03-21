cc86efd83dbf3aab67aa79a8a29d95c5d7a244ea
♠ Levi Perez <levi@leviperez.dev>
2023-03-21 16:58:08 -0700

    added test: OreEventsInEditor (still need runtime tests for testing persistence)

---
+++ Tests/Editor/KoreEventsInEditor.cs
@@ -0,0 +1,38 @@
+/*! @file       Tests/Editor/OreEventsInEditor.cs
+ *  @author     Levi Perez (levi\@leviperez.dev)
+ *  @date       2023-03-21
+**/
+
+using Ore;
+
+using NUnit.Framework;
+
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.TestTools;
+
+
+internal static class OreEventsInEditor
+{
+
+  public static void StaticMethod()
+  {
+    Debug.Log("shmee.");
+  }
+
+
+  [Test]
+  public static void AddPersistentStatic()
+  {
+    var @event = new VoidEvent(isEnabled: true);
+
+    @event.AddPersistent(StaticMethod);
+
+    LogAssert.Expect(LogType.Log, "schmee.");
+
+    Assert.True(@event.TryInvoke());
+
+    Assert.Pass("no news is good news");
+  }
+
+} // end class OreEventsInEditor
