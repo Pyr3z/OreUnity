f886b9755c6f14240dc139da7e3a86d3aa677282
♠ Levi Perez <levi@leviperez.dev>
2023-04-12 20:52:00 -0700

    TypeMembers: nullability annotations trump kassertions

--- Static/TypeMembers.cs
+++ Static/TypeMembers.cs
@@ -8,8 +8,9 @@
 
 using System.Reflection;
 using System.Collections.Generic;
-
 using UnityEngine;
+
+using JetBrains.Annotations;
 
 using Type = System.Type;
 
@@ -50,18 +51,14 @@ namespace Ore
                                             BindingFlags.Public;
 
 
-    public static bool IsDefined<T>(this MemberInfo member, bool inherit = true)
+    public static bool IsDefined<T>([NotNull] this MemberInfo member, bool inherit = true)
       where T : System.Attribute
     {
-      OAssert.NotNull(member, "member");
-
       return member.IsDefined(typeof(T), inherit);
     }
 
-    public static bool AreAnyDefined(this MemberInfo member, IEnumerable<Type> attributes, bool inherit = true)
+    public static bool AreAnyDefined([NotNull] this MemberInfo member, [NotNull] IEnumerable<Type> attributes, bool inherit = true)
     {
-      OAssert.AllNotNull(member, attributes);
-
       foreach (var attr in attributes)
       {
         if (member.IsDefined(attr, inherit))
@@ -71,36 +68,51 @@ namespace Ore
       return false;
     }
 
-    public static bool IsHidden(this FieldInfo field)
+    public static bool IsHidden([NotNull] this FieldInfo field)
     {
-      OAssert.NotNull(field, "field");
-
       return field.IsDefined<HideInInspector>() ||
              field.IsDefined<System.ObsoleteAttribute>();
     }
 
+    public static bool IsArrayOrList([NotNull] this Type type)
+    {
+      return type.IsArray || ( type.IsGenericType && type.GetGenericTypeDefinition() == typeof(List<>) );
+    }
 
-    public static bool TryGetField(this Type type, string name, out FieldInfo field, BindingFlags blags = INSTANCE)
+    public static bool IsUnityStruct([NotNull] this Type type)
+    {
+      while (IsArrayOrList(type))
+      {
+        type = type.IsArray ? type.GetElementType() : type.GetGenericArguments()[0];
+        if (type is null)
+          return false;
+      }
+
+      return type.IsValueType && ( type.Namespace?.StartsWith("Unity") ?? false );
+    }
+
+
+    public static bool TryGetField([CanBeNull] this Type type, [NotNull] string name, out FieldInfo field, BindingFlags blags = INSTANCE)
     {
       field = type?.GetField(name, blags);
       return field != null;
     }
 
-    public static bool TryGetSerializableField(this Type type, string name, out FieldInfo field)
+    public static bool TryGetSerializableField([CanBeNull] this Type type, [NotNull] string name, out FieldInfo field)
     {
-      return type.TryGetField(name, out field, INSTANCE) &&
-             (field.IsPublic || field.IsDefined<SerializeField>());
+      return TryGetField(type, name, out field, INSTANCE) &&
+             ( field.IsPublic || field.IsDefined<SerializeField>() );
     }
 
-    public static bool TryGetInternalField(this Type type, string name, out FieldInfo field)
+    public static bool TryGetInternalField([CanBeNull] this Type type, [NotNull] string name, out FieldInfo field)
     {
-      return type.TryGetField(name, out field, NONPUBLIC);
+      return TryGetField(type, name, out field, NONPUBLIC);
     }
 
 
-    public static bool TryGetStaticValue<T>(this Type type, string name, out T value)
+    public static bool TryGetStaticValue<T>([CanBeNull] this Type type, [NotNull] string name, out T value)
     {
-      if (type.TryGetField(name, out var field, STATIC) &&
+      if (TryGetField(type, name, out var field, STATIC) &&
           typeof(T).IsAssignableFrom(field.FieldType))
       {
         value = (T)field.GetValue(null);
